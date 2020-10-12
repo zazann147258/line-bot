0c0ca4888feb9b6150ee0bb452910db846358341
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2020-10-12 21:52:06 +0700

    Update lineapi.php

--- lineapi.php
+++ lineapi.php
@@ -1,7 +1,7 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = '85Ve118k5nIckQt7WU7TABbHNMvXGsiOfBE2iXk4ffVUwdJpqkGEt7hXI2Prw3QCKkDSJxH4eK+c6eKoUQARkhi8wvjekDKmA0yRzqoiiiehBWyOduoU9DrWnIeHqt75zPCCHudXXx0Etona7yNscQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'PZ6qlbYABvcIg+sly4KFcjs8rAVOW1+EEEDBgcOn86a9MwA+MNHV8//FPERaqcVuWnKEs4U+6oe0jLA++fQlGKdK9/SCRKlZ0x4otRbscQZBRbe5VDkXvu32iZAA+dpXEwrb47Ncr9kuH1vSp+t3LwdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
