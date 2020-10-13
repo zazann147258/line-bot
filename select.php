53b685c0025e75a95388be7e6dcf099375a36f53
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2020-10-13 21:32:50 +0700

    Update select.php

--- select.php
+++ select.php
@@ -8,7 +8,7 @@ curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $result = curl_exec($ch);
 curl_close($ch);   		   
-$obj = json_decode($result);		   
+//$obj = json_decode($result);		   
 
 echo $result;
 
