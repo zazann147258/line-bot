f76f046d8c0d16226d429d7bb4633c85929863a9
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2020-10-13 08:48:11 +0700

    Update lineapi.php

--- lineapi.php
+++ lineapi.php
@@ -3,7 +3,7 @@
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 	$protocol = (isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0');
 	$code = 405;
-	$text = "Accept \"POST\" Method requests";
+	$text = 'Accept "POST" Method requests';
 	header($protocol . ' ' . $code . ' ' . $text);
 	exit();
 }
