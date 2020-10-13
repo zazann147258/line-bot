61eed0154991156f07449d73bad6e725e7172301
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2020-10-13 17:53:54 +0700

    Update lineapi_mysql.php

--- lineapi_mysql.php
+++ lineapi_mysql.php
@@ -59,18 +59,16 @@ function mySQL($url, $post_body)
 	
 	//$result = file_get_contents($url, false, $context);
 		
-	//$result = file_get_contents($url);
+	$result = file_get_contents($url);
 	
-	$ch = curl_init($url);	
-	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, 'Content-Type: application/json');
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	
-	$result = curl_exec($ch);
-	
-	curl_close($ch);
+	//$ch = curl_init($url);	
+	//curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	//curl_setopt($ch, CURLOPT_HTTPHEADER, 'Content-Type: application/json');
+	//curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+	//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);	
+	//$result = curl_exec($ch);	
+	//curl_close($ch);
 	
 	return $result;
 }
