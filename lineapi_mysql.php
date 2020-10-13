c56743742268cf3891ec901346b289060813a3f4
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2020-10-13 16:21:47 +0700

    Add files via upload

---
+++ lineapi_mysql.php
@@ -0,0 +1,65 @@
+<?php
+
+$channelAccessToken = 'PZ6qlbYABvcIg+sly4KFcjs8rAVOW1+EEEDBgcOn86a9MwA+MNHV8//FPERaqcVuWnKEs4U+6oe0jLA++fQlGKdK9/SCRKlZ0x4otRbscQZBRbe5VDkXvu32iZAA+dpXEwrb47Ncr9kuH1vSp+t3LwdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+
+$request = file_get_contents('php://input');   // Get request content
+
+$request_json = json_decode($request, true);   // Decode JSON request
+
+foreach ($request_json['events'] as $event)
+{
+	if ($event['type'] == 'message') 
+	{
+		if($event['message']['type'] == 'text')
+		{
+			$text = $event['message']['text'];
+			
+			$reply_message = 'ระบบได้รับข้อความ '. $text.' ของคุณแล้ว!';   
+			
+		} else {
+			$reply_message = 'ระบบได้รับ '.$event['message']['type'].' ของคุณแล้ว!';
+		}
+	} else {
+		$reply_message = 'ระบบได้รับ Event '.$event['type'].' ของคุณแล้ว!';
+	}
+	
+	// reply message
+	$post_header = array('Content-Type: application/json', 'Authorization: Bearer ' . $channelAccessToken);
+	$data = ['replyToken' => $event['replyToken'], 'messages' => [['type' => 'text', 'text' => $reply_message]]];
+	$post_body = json_encode($data);
+	$send_result = replyMessage('https://api.line.me/v2/bot/message/reply', $post_header, $post_body);
+	//$send_result = send_reply_message('https://api.line.me/v2/bot/message/reply', $post_header, $post_body);
+}
+
+function replyMessage($url, $post_header, $post_body)
+{
+        $context = stream_context_create([
+            'http' => [
+                'method' => 'POST',
+                'header' => $post_header,
+                'content' => $post_body,
+            ],
+        ]);
+	
+	$result = file_get_contents($url, false, $context);
+
+	return $result;
+}
+
+function send_reply_message($url, $post_header, $post_body)
+{
+	$ch = curl_init($url);	
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	
+	$result = curl_exec($ch);
+	
+	curl_close($ch);
+	
+	return $result;
+}
+
+?>
