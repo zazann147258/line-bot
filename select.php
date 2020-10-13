a3387ecacf92d7a003b7b5a9fc63cd1d9e9ad5b7
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2020-10-13 21:23:38 +0700

    Update select.php

--- select.php
+++ select.php
@@ -4,12 +4,9 @@ $url = 'http://bot.kantit.com/select.php';
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);   		   
-$obj = json_decode($result);		   
+//$obj = json_decode($result);		   
 
 echo $result;
 
