e797b15cdb4de6b594061b05e99fa33ec8818480
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2020-10-13 22:51:56 +0700

    Update select.php

--- select.php
+++ select.php
@@ -8,8 +8,10 @@ $result_json = json_decode($result, true);
 
 //var_dump($result_json);
 
-foreach($result_json as $key => $value) {
-  echo $key . " => " . $value . "<br>";
+foreach($result_json as $key => $values) {
+  var_dump($values);
+  echo "<br>";
+  //echo $key . " => " . $value . "<br>";
 }
 
 //foreach($result_json->id as $data)
