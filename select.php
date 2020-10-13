6abebc0a3c1fe3371f05881c005ffd899b543193
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2020-10-13 23:26:08 +0700

    Update select.php

--- select.php
+++ select.php
@@ -12,7 +12,7 @@ $data = array();
 
 foreach($result_json as $values) {
   
-  echo $values->msg_id . " " . $values->msg_from . “<br>”;
+  echo $values['msg_id'] . " " . $values['msg_from'] . “<br>”;
   
   //var_dump($values); echo "<br>";
   
