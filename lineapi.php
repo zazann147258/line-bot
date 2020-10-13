8b6e5d90786c94cb27e11e76df2d6b4f362998c3
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2020-10-13 11:55:08 +0700

    Update lineapi.php

--- lineapi.php
+++ lineapi.php
@@ -1,32 +1,34 @@
 <?php
 
+$API_URL = 'https://api.line.me/v2/bot/message/reply';
 $ACCESS_TOKEN = 'PZ6qlbYABvcIg+sly4KFcjs8rAVOW1+EEEDBgcOn86a9MwA+MNHV8//FPERaqcVuWnKEs4U+6oe0jLA++fQlGKdK9/SCRKlZ0x4otRbscQZBRbe5VDkXvu32iZAA+dpXEwrb47Ncr9kuH1vSp+t3LwdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
-$post_header = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
+$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
 
-foreach ($request_array['events'] as $event)
+if ( sizeof($request_array['events']) > 0 )
 {
-	$reply_message = '';
-	$reply_token = $event['replyToken'];
+
+ foreach ($request_array['events'] as $event)
+ {
+  $reply_message = '';
+  $reply_token = $event['replyToken'];
 
   if ( $event['type'] == 'message' ) 
   {
    
    if( $event['message']['type'] == 'text' )
    {
-		$text = $event['message']['text'];
-
-		//$reply_message = 'ระบบได้รับข้อความ '.$text.' ของคุณแล้ว';  
-	   $reply_message = 'ระบบได้รับข้อความ 1.'.$text.' ของคุณแล้ว';
+	   $text = $event['message']['text'];
+	   $reply_message = '('.$text.') ได้รับข้อความเรียบร้อย!!';   
    }
    else
-    $reply_message = 'ระบบได้รับ '.$event['message']['type'].' ของคุณแล้ว';
+    $reply_message = 'ระบบได้รับ '.ucfirst($event['message']['type']).' ของคุณแล้ว';
   
   }
   else
-   $reply_message = 'ระบบได้รับ Event '.$event['type'].' ของคุณแล้ว';
+   $reply_message = 'ระบบได้รับ Event '.ucfirst($event['type']).' ของคุณแล้ว';
  
   if( strlen($reply_message) > 0 )
   {
@@ -37,14 +39,15 @@ foreach ($request_array['events'] as $event)
    ];
    $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
 
-   $send_result = send_reply_message($post_body);
+   $send_result = send_reply_message($API_URL, $POST_HEADER, $post_body);
    echo "Result: ".$send_result."\r\n";
   }
  }
+}
 
-function send_reply_message($post_body)
-{	
- $ch = curl_init('https://api.line.me/v2/bot/message/reply/');
+function send_reply_message($url, $post_header, $post_body)
+{
+ $ch = curl_init($url);
  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
  curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
