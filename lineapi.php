9cbe42c6d736acae9f64873a5d91c2719728fed0
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2020-10-13 10:46:36 +0700

    Update lineapi.php

--- lineapi.php
+++ lineapi.php
@@ -23,64 +23,9 @@ if ( sizeof($request_array['events']) > 0 )
 		
 	   	if($text == "ชื่อ" || $text == "ชื่ออะไร" || $text == "ชื่ออะไรครับ"|| $text == "ชื่ออะไรคะ"){
 			$reply_message = 'ชื่อของฉันคือ BOTCAT';
+		}else{
+		$reply_message = 'ระบบได้รับ '.ucfirst($event['message']['text']).' ของคุณแล้ว';
 		}
-	   
-	   	if($text == "สถานการณ์โควิดวันนี้" || $text == "covid19" || $text == "covid-19" || $text == "Covid-19"){
-		   $url = 'https://covid19.th-stat.com/api/open/today';
-		   $ch = curl_init($url);
-		   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-		   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		   curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
-		   curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
-		   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-		   $result = curl_exec($ch);
-		   curl_close($ch);   
-		   
-		   $obj = json_decode($result);
-		   
-		   //$reply_message = $result;
-		   $reply_message = 'ติดเชื้อสะสมโว้ยยย!!! '. $obj->{'Confirmed'} .' คน รักษาหายแล้ว '.$obj->{'Recovered'} . ' คน';
-	
-	        }	
-	   
-	   if($text =="@บอท ขอรหัสนิสิตของผู้พัฒนา ส่งไปที่ https://linebot.kantit.com/stuid.php"){
-	    	  $url = 'https://linebot.kantit.com/stuid.php';
-		   $ch = curl_init($url);
-		   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-		   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		   curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
-		   curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
-		   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-		   $result = curl_exec($ch);
-		   curl_close($ch);   		   
-		   $obj = json_decode($result);		   
-		   $reply_message = $result;
-		   //$reply_message = 'ผลการบันทึกข้อมูล'. $obj->{'status'} .' และ '.$obj->{'data'} . ' OK!';
-	   }
-	   
-	   if($text =="@บอท ขอรายชื่อนิสิตที่ส่งงาน LineBoT"){
-	    	   $url = 'https://linebot.kantit.com/list.php';
-		   
-		   $reply_message = file_get_contents($url);   // Get request content
-		   
-		   //$request_array = json_decode($request, true);   // Decode JSON to Array
-		   
-		   //$result = file_get_contents($url);		   
-		   //$obj = json_decode($result);
-		   //$reply_message = 'มีส่งงาน '. $obj->{'Confirmed'} .' คน ได้แก่...';
-		   //$reply_message = 'ติดเชื้อสะสม '. $obj->{'Confirmed'} .' คน รักษาหายแล้ว '.$obj->{'Recovered'} . ' คน';
-		   $reply_message = "โปรดรอสักครู่....";
-	   }
-	   
-	     if($text =="@บอท ขอที่อยู่มทร.หน่อยจิ"){
-	    	   $url = 'https://www.google.com/maps/place/Phra+Nakhon+Si+Ayutthaya/@14.3935691,100.237786,10z/data=!3m1!4b1!4m8!1m2!2m1!1smaps+google!3m4!1s0x30e2736f5bfd8f7f:0x1019237450c4860!8m2!3d14.3532128!4d100.5689599';
-		   
-		   $reply_message = $url;   // Get request content
-
-		   //$reply_message = $reply_message;
-	   }
-	   
-		//$reply_message = '('.$text.') ได้รับข้อความเรียบร้อย!!';   
    }
    else
     $reply_message = 'ระบบได้รับ '.ucfirst($event['message']['type']).' ของคุณแล้ว';
