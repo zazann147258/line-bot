c7c497d119f2aa8c1af1d9f0584fd35071565bcd
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2020-10-13 21:55:37 +0700

    Update select.php

--- select.php
+++ select.php
@@ -3,12 +3,14 @@
 $url = 'https://bot.kantit.com/select.php';
 //$url = 'https://covid19.th-stat.com/api/open/today';
 
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$result = curl_exec($ch);
-curl_close($ch);   		   
+//$ch = curl_init($url);
+//curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//$result = curl_exec($ch);
+//curl_close($ch);   		   
 //$obj = json_decode($result);		   
+
+$result = file_get_contents($url);
 
 echo $result;
 
