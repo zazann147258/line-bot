3473c43a54ed7c59642d28be0c19d96ba8b8effa
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2020-10-13 08:53:17 +0700

    Update lineapi.php

--- lineapi.php
+++ lineapi.php
@@ -1,11 +1,20 @@
 <?php
 
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-	$protocol = (isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0');
+$protocol = (isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0');
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {	
 	$code = 405;
 	$text = 'Accept "POST" Method requests';
 	header($protocol . ' ' . $code . ' ' . $text);
 	exit();
 }
+
+if($_SERVER['CONTENT_TYPE'] != 'application/json'){
+	$code = 444;
+	$text = 'Accept "JSON" body requests';
+	header($protocol . ' ' . $code . ' ' . $text);
+        exit();
+    }
+
 
 ?>
