af82019a5f3aa85eba7e70a11de1a0b68a681a36
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2020-10-13 23:15:04 +0700

    Update select.php

--- select.php
+++ select.php
@@ -10,16 +10,16 @@ $result_json = json_decode($result, true);
 
 $data = array();
 
-foreach($result_json as $key => $values) {
-  //var_dump($values); echo "<br>";
+foreach($result_json as $values) {
+  var_dump($values); echo "<br>";
   foreach($values as $key => $value) {
     //var_dump($value); 
     //echo $key . " => " . $value . "<br>";
-    array_push($data, array("fullname"=>$value['msg_from']));
+    //array_push($data, array("fullname"=>$value['msg_from']));
   }
   //echo "<br>";  
 }
 
-var_dump($data);
+//var_dump($data);
 
 ?>
