d76fbaca3c64f399f1b2e5f4ee83c795ebbd99be
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2020-10-13 11:27:10 +0700

    Update lineapi.php

--- lineapi.php
+++ lineapi.php
@@ -2,7 +2,6 @@
 
 $ACCESS_TOKEN = 'PZ6qlbYABvcIg+sly4KFcjs8rAVOW1+EEEDBgcOn86a9MwA+MNHV8//FPERaqcVuWnKEs4U+6oe0jLA++fQlGKdK9/SCRKlZ0x4otRbscQZBRbe5VDkXvu32iZAA+dpXEwrb47Ncr9kuH1vSp+t3LwdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
@@ -39,15 +38,17 @@ if ( sizeof($request_array['events']) > 0 )
    ];
    $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
 
-   $send_result = send_reply_message($API_URL, $POST_HEADER, $post_body);
+   $send_result = send_reply_message($post_body);
    echo "Result: ".$send_result."\r\n";
   }
  }
 }
 
-function send_reply_message($url, $post_header, $post_body)
+function send_reply_message($post_body)
 {
- $ch = curl_init($url);
+	$post_header = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
+
+ $ch = curl_init($API_URL);
  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
  curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
