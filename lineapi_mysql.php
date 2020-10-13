f1c6a426289abda4c0bb1ba83a6e937730930a0d
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2020-10-13 17:24:08 +0700

    Update lineapi_mysql.php

--- lineapi_mysql.php
+++ lineapi_mysql.php
@@ -59,17 +59,29 @@ function mySQL($url, $post_header, $post_body)
 
 function replyMessage($url, $post_header, $post_body)
 {
-        $context = stream_context_create([
-            'http' => [
-                'method' => 'POST',
-                'header' => $post_header,
-                'content' => $post_body,
-            ],
-        ]);
+        //$context = stream_context_create([
+        //    'http' => [
+        //        'method' => 'POST',
+        //        'header' => $post_header,
+        //        'content' => $post_body,
+        //    ],
+        //]);
 	
-	$result = file_get_contents($url, false, $context);
+	//$result = file_get_contents($url, false, $context);
 
-	return $result;
+		   $ch = curl_init($url);
+		   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		   curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+		   curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+		   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		   $result = curl_exec($ch);
+		   curl_close($ch);   		   
+		   $obj = json_decode($result);
+	
+	
+
+	return $obj;
 }
 
 function send_reply_message($url, $post_header, $post_body)
