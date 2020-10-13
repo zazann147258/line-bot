663a6343369e0422e54f63ead45813611524193d
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2020-10-13 21:23:11 +0700

    Update mysql.php

--- mysql.php
+++ mysql.php
@@ -4,14 +4,11 @@ $url = 'http://bot.kantit.com/insert_json.php';
 		   $ch = curl_init($url);
 		   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 		   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		   curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
-		   curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
-		   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 		   $result = curl_exec($ch);
 		   curl_close($ch);   		   
-		   $obj = json_decode($result);		   
+		   //$obj = json_decode($result);		   
 
-echo $obj;
+echo $result;
 
 
 ?>
