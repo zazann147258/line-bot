aaf522dd5952c1c4c9469e1c1acabe3c5a1568d1
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2020-10-14 12:01:58 +0700

    Update json_select.php

--- json_select.php
+++ json_select.php
@@ -6,12 +6,12 @@ $result = file_get_contents($url);
 
 $result_json = json_decode($result, true); //var_dump($result_json);
 
-$data = "";
-		
-	foreach($result_json as $values) {
-		$data .= $values["user_stuid"] . " " . $values["user_firstname"] . " " . $values["user_lastname"] . "\r\n";
-	}
+//$data = "";
 
-echo $data;
+foreach($result_json as $values) {
+	echo $values["user_stuid"] . " " . $values["user_firstname"] . " " . $values["user_lastname"] . "\r\n";
+}
+
+//echo $data;
 
 ?>
