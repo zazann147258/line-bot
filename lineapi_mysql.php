a2a837a9b87602af0a2fba0b85ae3f0bc13e7a45
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2020-10-14 10:30:36 +0700

    Update lineapi_mysql.php

--- lineapi_mysql.php
+++ lineapi_mysql.php
@@ -14,7 +14,11 @@ foreach ($request_json['events'] as $event)
 		{
 			$text = $event['message']['text'];
 			
-			$reply_message = 'ระบบได้รับข้อความ '. $text.' ของคุณแล้ว!';   
+			$reply_message = 'ระบบได้รับข้อความ '. $text.' ของคุณแล้ว!';
+			
+			if($text == "@บอท ขอรายชื่อนิสิตทั้งหมด"){
+				$reply_message = mySQL_select('http://bot.kantit.com/select_users.php');
+			}
 			
 		} else {
 			$reply_message = 'ระบบได้รับ '.$event['message']['type'].' ของคุณแล้ว!';
@@ -25,7 +29,7 @@ foreach ($request_json['events'] as $event)
 	
 	
 	
-	$reply_message = mySQL_select('http://bot.kantit.com/select_users.php');
+	
 	
 	
 	
