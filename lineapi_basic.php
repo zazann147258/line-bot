f747ecab474b22ef7f71458218b61e2c1c726473
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2020-10-14 11:37:24 +0700

    Update lineapi_basic.php

--- lineapi_basic.php
+++ lineapi_basic.php
@@ -14,13 +14,13 @@ foreach ($request_json['events'] as $event)
 		{
 			$text = $event['message']['text'];
 			
-			$reply_message = 'ระบบได้รับข้อความ '. $text.' ของคุณแล้ว!';   
+			$reply_message = 'ฉันได้รับข้อความ '. $text.' ของคุณแล้ว!';   
 			
 		} else {
-			$reply_message = 'ระบบได้รับ '.$event['message']['type'].' ของคุณแล้ว!';
+			$reply_message = 'ฉันได้รับ '.$event['message']['type'].' ของคุณแล้ว!';
 		}
 	} else {
-		$reply_message = 'ระบบได้รับ Event '.$event['type'].' ของคุณแล้ว!';
+		$reply_message = 'ฉันได้รับ Event '.$event['type'].' ของคุณแล้ว!';
 	}
 	
 	// reply message
