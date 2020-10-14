fedf006e0bc72bc00a5fdb83a4340d1f328a0558
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2020-10-14 12:16:37 +0700

    Update json_insert.php

--- json_insert.php
+++ json_insert.php
@@ -1,14 +1,14 @@
 <?php
 
 $url = 'http://bot.kantit.com/insert_json.php';
-		   $ch = curl_init($url);
-		   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-		   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		   $result = curl_exec($ch);
-		   curl_close($ch);   		   
-		   //$obj = json_decode($result);		   
 
-echo $result;
+//$ch = curl_init($url);
+//url_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//$result = curl_exec($ch);
+//curl_close($ch);   		   
+//$obj = json_decode($result);		   
+//echo $result;
 
 
 ?>
