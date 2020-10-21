040131ff0ebf408dbdc461dd59a3cd7e6e643381
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2020-10-21 09:02:31 +0700

    Update linebot_api.php

--- linebot_api.php
+++ linebot_api.php
@@ -20,11 +20,11 @@ foreach ($request_json['events'] as $event)
 			
 			if($txts[0] == "@บอท"){
 				
-				$reply_message = "กรุณาใช้รูปแบบคำสั่งที่ถูกต้องงงงง!!";
+				$reply_message = "กรุณาใช้รูปแบบคำสั่งที่ถูกต้องงงงง!!\n";
 				
-				$reply_message .= "ฉันมีบริการให้คุณสั่งได้ ดังนี้...";
+				$reply_message .= "ฉันมีบริการให้คุณสั่งได้ ดังนี้...\n";
 				
-				$reply_message .= 'พิมพ์ว่า "@บอท ขอรายชื่อนิสิตทั้งหมด"';
+				$reply_message .= 'พิมพ์ว่า "@บอท ขอรายชื่อนิสิตทั้งหมด\n"';
 				
 				if($txts[1] == "ขอรายชื่อนิสิตทั้งหมด"){
 					$reply_message = mySQL_selectAll('http://bot.kantit.com/json_select_users.php');
