834c7632dea54f8a7146b54acbed89ac7133ff9b
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2020-10-21 10:20:09 +0700

    Update linebot_api.php

--- linebot_api.php
+++ linebot_api.php
@@ -24,9 +24,9 @@ foreach ($request_json['events'] as $event)
 				
 				$reply_message .= "ฉันมีบริการให้คุณสั่งได้ ดังนี้...\n";
 				
-				$reply_message .= 'พิมพ์ว่า "@บอท ขอรายชื่อนิสิตทั้งหมด"\n';
-				$reply_message .= 'พิมพ์ว่า "@บอท ขอรายชื่อนิสิต รหัส 61160xxx"\n';
-				$reply_message .= 'พิมพ์ว่า "@บอท ขอรหัส FTP ของ s61160xxx"\n';
+				$reply_message .= "พิมพ์ว่า \"@บอท ขอรายชื่อนิสิตทั้งหมด\"\n";
+				$reply_message .= "พิมพ์ว่า \"@บอท ขอรายชื่อนิสิต รหัส 61160xxx\"\n";
+				$reply_message .= "พิมพ์ว่า \"@บอท ขอรหัส FTP ของ s61160xxx\"\r\n";
 				
 				if($txts[1] == "ขอรายชื่อนิสิตทั้งหมด"){
 					$reply_message = mySQL_selectAll('http://bot.kantit.com/json_select_users.php');
