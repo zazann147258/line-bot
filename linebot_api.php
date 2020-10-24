4da560b8397dece7bbbc1452adcbe174ef4e35eb
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2020-10-24 17:45:04 +0700

    Update linebot_api.php

--- linebot_api.php
+++ linebot_api.php
@@ -31,7 +31,7 @@ foreach ($request_json['events'] as $event)
 		   $obj = json_decode($result);
 		   
 		   $reply_message = $result;
-		   $reply_message = 'ติดเชื้อสะสม '. $obj->{'Confirmed'};
+		   $reply_message = 'จำนวนผู้เสียชีวิต'. $obj->{'Deaths'}.' คน.';
 	   }
 			if($text == "@บอท สวัสดี"){
 					$reply_message = "สวัสดีจ๊ะ++ ยินดีให้บริการ";
