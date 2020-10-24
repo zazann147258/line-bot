b1ae102c8be9f536dbe576d2ffd9f3f57556c725
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2020-10-24 17:34:23 +0700

    Update linebot_api.php

--- linebot_api.php
+++ linebot_api.php
@@ -17,7 +17,22 @@ foreach ($request_json['events'] as $event)
 			$reply_message = 'ฉันได้รับข้อความ "'. $text . '" ของคุณแล้ว!';
 			
 			$txts = explode(" ", $text);
-			
+			  if($text == "สถานการณ์โควิดวันนี้" || $text == "covid19" || $text == "covid-19" || $text == "Covid-19"){
+		   $url = 'https://covid19.th-stat.com/api/open/today';
+		   $ch = curl_init($url);
+		   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		   curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+		   curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+		   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		   $result = curl_exec($ch);
+		   curl_close($ch);   
+		   
+		   $obj = json_decode($result);
+		   
+		   $reply_message = $result;
+		   $reply_message = 'ติดเชื้อสะสม '. $obj->{'Confirmed'};
+	   }
 			if($text == "@บอท สวัสดี"){
 					$reply_message = "สวัสดีจ๊ะ++ ยินดีให้บริการ";
 				}
