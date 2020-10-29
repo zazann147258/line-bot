f38653ae52e6e3044a99670708fa1333b2342dbb
zazann147258 <70052039+zazann147258@users.noreply.github.com>
2020-10-29 16:49:19 +0700

    Update linebot_basic.php

--- linebot_basic.php
+++ linebot_basic.php
@@ -75,7 +75,7 @@ function mySQL_selectAll($url)
  $data = "ผลลัพธ์:\r\n";
   
  foreach($result_json as $values) {
-  $data .= $values["stuid"] . " " . $values["full_name"] . " " .  "\r\n";
+  $data += $values["stuid"] . " " . $values["full_name"] . " " .  "\r\n";
  }
  
  return $data;
