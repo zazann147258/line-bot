e68e477dc0474a302ffd035bd872e0c41da8884e
zazann147258 <70052039+zazann147258@users.noreply.github.com>
2020-10-29 16:31:27 +0700

    Update linebot_basic.php

--- linebot_basic.php
+++ linebot_basic.php
@@ -1,6 +1,6 @@
 <?php
 
-$channelAccessToken = 'PZ6qlbYABvcIg+sly4KFcjs8rAVOW1+EEEDBgcOn86a9MwA+MNHV8//FPERaqcVuWnKEs4U+6oe0jLA++fQlGKdK9/SCRKlZ0x4otRbscQZBRbe5VDkXvu32iZAA+dpXEwrb47Ncr9kuH1vSp+t3LwdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$channelAccessToken = 'qQJOynpqEHSDLcD52g15om6bZjx0HYWEjvRvHnaFkCWwL+hTD1p+VieN1fx8V4IZg9KdB90QcHuea/80mItEpG9QskU4H5czwjCMjif9Ck6cRlzP4jHPOQXo4RQ8wY+MXH1Y1uqUgMBh4kwCFP+SOAdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 
 $request = file_get_contents('php://input');   // Get request content
 
@@ -8,27 +8,31 @@ $request_json = json_decode($request, true);   // Decode JSON request
 
 foreach ($request_json['events'] as $event)
 {
-	if ($event['type'] == 'message') 
-	{
-		if($event['message']['type'] == 'text')
-		{
-			$text = $event['message']['text'];
-			
-			$reply_message = 'ฉันได้รับข้อความ '. $text.' ของคุณแล้ว!';   
-			
-		} else {
-			$reply_message = 'ฉันได้รับ '.$event['message']['type'].' ของคุณแล้ว!';
-		}
-	} else {
-		$reply_message = 'ฉันได้รับ Event '.$event['type'].' ของคุณแล้ว!';
-	}
-	
-	// reply message
-	$post_header = array('Content-Type: application/json', 'Authorization: Bearer ' . $channelAccessToken);
-	$data = ['replyToken' => $event['replyToken'], 'messages' => [['type' => 'text', 'text' => $reply_message]]];
-	$post_body = json_encode($data);
-	$send_result = replyMessage('https://api.line.me/v2/bot/message/reply', $post_header, $post_body);
-	//$send_result = send_reply_message('https://api.line.me/v2/bot/message/reply', $post_header, $post_body);
+ if ($event['type'] == 'message') 
+ {
+  if($event['message']['type'] == 'text')
+  {
+   $text = $event['message']['text'];
+   
+   $reply_message = 'ฉันได้รับข้อความ '. $text.' ของคุณแล้ว!';   
+   
+   $reply_message = 'Jakkarin';  
+   
+   $reply_message = mySQL_selectAll('http://s61160172.kantit.com/json_select.php');
+   
+  } else {
+   $reply_message = 'ฉันได้รับ '.$event['message']['type'].' ของคุณแล้ว!';
+  }
+ } else {
+  $reply_message = 'ฉันได้รับ Event '.$event['type'].' ของคุณแล้ว!';
+ }
+ 
+ // reply message
+ $post_header = array('Content-Type: application/json', 'Authorization: Bearer ' . $channelAccessToken);
+ $data = ['replyToken' => $event['replyToken'], 'messages' => [['type' => 'text', 'text' => $reply_message]]];
+ $post_body = json_encode($data);
+ $send_result = replyMessage('https://api.line.me/v2/bot/message/reply', $post_header, $post_body);
+ //$send_result = send_reply_message('https://api.line.me/v2/bot/message/reply', $post_header, $post_body);
 }
 
 function replyMessage($url, $post_header, $post_body)
@@ -40,26 +44,41 @@ function replyMessage($url, $post_header, $post_body)
                 'content' => $post_body,
             ],
         ]);
-	
-	$result = file_get_contents($url, false, $context);
+ 
+ $result = file_get_contents($url, false, $context);
 
-	return $result;
+ return $result;
 }
 
 function send_reply_message($url, $post_header, $post_body)
 {
-	$ch = curl_init($url);	
-	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	
-	$result = curl_exec($ch);
-	
-	curl_close($ch);
-	
-	return $result;
+ $ch = curl_init($url); 
+ curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+ curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+ curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+ 
+ $result = curl_exec($ch);
+ 
+ curl_close($ch);
+ 
+ return $result;
+}
+
+function mySQL_selectAll($url)
+{
+ $result = file_get_contents($url);
+ 
+ $result_json = json_decode($result, true); //var_dump($result_json);
+ 
+ $data = "ผลลัพธ์:\r\n";
+  
+ foreach($result_json as $values) {
+  $data .= $values["stuid"] . " " . $values["fullname"] . " " .  "\r\n";
+ }
+ 
+ return $data;
 }
 
 ?>
